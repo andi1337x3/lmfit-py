1c9664cfe7998cf916a5d401e372efe09c71b52e
araab <a.raabgrund@gmail.com>
2021-09-01 10:25:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,20 +24,21 @@ class BinaryDistribution(Distribution):
 EXCLUDED_PACKAGES = []
 
 setup(name='lmfit',
+      version="0.0.1", # modified by araab
       #use_scm_version={
       #    'write_to': 'lmfit/version.py',
       #    'version_scheme': 'post-release'},
       author='LMFit Development Team',
       author_email='matt.newville@gmail.com',
       url='https://lmfit.github.io/lmfit-py/',
-      # download_url='https://lmfit.github.io//lmfit-py/',
+      download_url='https://lmfit.github.io//lmfit-py/',
       setup_requires=['setuptools_scm'],
       install_requires=['asteval>=0.9.22',
                         'numpy>=1.18',
                         'scipy>=1.3',
                         'uncertainties>=3.0.1'],
       python_requires='>=3.6',
-      license='BSD',
+      license='BSD-3',
       description="Least-Squares Minimization with Bounds and Constraints",
       #long_description=long_desc,
       platforms=['Windows', 'Linux', 'Mac OS X'],
